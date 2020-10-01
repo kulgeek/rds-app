565e1eda471095759d9326d216cf5ba482a1bf93
Isaack okello brian <isaackokello@gmail.com>
2020-10-01 18:43:22 +0200

    Commiting webpack config

--- next.config.js
+++ next.config.js
@@ -6,21 +6,30 @@ const path = require("path");
 const isProd = process.env.NODE_ENV === "production";
 
 // fix: prevents error when .less files are required by node
+
+/* webpack: {
+ 	config.resolve.alias["components"] = path.join(__dirname, "components");
+     config.resolve.alias["public"] = path.join(__dirname, "public");
+}
+*/
+
+//trying to add this webpack code block above in the block below but i don't know how the syntax works
 if (typeof require !== "undefined") {
 	require.extensions[".less"] = (file) => {};
 }
 
-module.exports = withCSS({
-	cssModules: true,
-	cssLoaderOptions: {
-		importLoaders: 1,
-		localIdentName: "[local]___[hash:base64:5]",
-	},
-	...withLess(
-		withSass({
-			lessLoaderOptions: {
-				javascriptEnabled: true,
-			},
-		})
-	),
-});
+module.exports = (config) =>
+	withCSS({
+		cssModules: true,
+		cssLoaderOptions: {
+			importLoaders: 1,
+			localIdentName: "[local]___[hash:base64:5]",
+		},
+		...withLess(
+			withSass({
+				lessLoaderOptions: {
+					javascriptEnabled: true,
+				},
+			})
+		),
+	});
